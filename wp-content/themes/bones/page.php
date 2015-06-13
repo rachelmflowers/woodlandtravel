40306188dea94854653d3d2074140597fbb960c7
Rachel Flowers <rachel@appicstudios.com>
2015-06-13 13:55:46 -0400

    styles

--- wp-content/themes/bones/page.php
+++ wp-content/themes/bones/page.php
@@ -1,86 +1,60 @@
 <?php get_header(); ?>
+        
+<?php
+if (have_posts()) : 
+    while (have_posts()) : the_post(); ?>
+    <style>
+        .breadcrumbs { display: none; }
+    </style>
+    
+    <header id="page-header">
+        <?php
+        if ( has_post_thumbnail() ) {
+        	the_post_thumbnail('full');
+        }
+        ?>
+    </header>
 
-
-
-	<style>
-
-	.breadcrumbs {display: none;}
-
-	</style>
-
-
-
-            <div id="content">
-
-
-
-                <div id="inner-content" class="wrap">
-
-			
-
-			<?php if ( function_exists('yoast_breadcrumb') ) yoast_breadcrumb('<p id="breadcrumbs">','</p>'); ?>
-
-
-
-                        <main id="main" class="m-all t-2of3 d-5of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
-
-
-
-                            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-
-
-                                <?php the_content(); ?>
-
-
-
-                            <?php endwhile; else : ?>
-
-
-
-                                    <article id="post-not-found" class="hentry cf">
-
-                                        <header class="article-header">
-
-                                            <h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
-
-                                        </header>
-
-                                        <section class="entry-content">
-
-                                            <p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
-
-                                        </section>
-
-                                        <footer class="article-footer">
-
-                                                <p><?php _e( 'This is the error message in the page.php template.', 'bonestheme' ); ?></p>
-
-                                        </footer>
-
-                                    </article>
-
-
-
-                            <?php endif; ?>
-
-
-
-                        </main>
-
-
-
-                        <?php get_sidebar(); ?>
-
-
-
-                </div>
-
-
-
+    <div id="content">
+        
+        <div id="inner-content" class="wrap cf">
+            <?php if ( function_exists('yoast_breadcrumb') ) yoast_breadcrumb('<p id="breadcrumbs">','</p>'); ?>
+            
+            <div class="m-all t-2of3 d-5of7">
+                <?php the_content(); ?>
             </div>
+            
+            <?php get_sidebar(); ?>
+        </div>
+        
+    </div>
+        
+    <?php
+    endwhile; 
+else : ?>
+            
+<div id="content">
+    
+    <div id="inner-content" class="wrap">
+        <article id="post-not-found" class="hentry cf">
+            <header class="article-header">
+                <h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
+            </header>
 
+            <section class="entry-content">
+                <p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
+            </section>
 
+            <footer class="article-footer">
+                <p><?php _e( 'This is the error message in the page.php template.', 'bonestheme' ); ?></p>
+            </footer>
+        </article>
+        
+        <?php get_sidebar(); ?>
+    </div>
+    
+</div>
+<?php endif; ?>
 
 <?php get_footer(); ?>
 
